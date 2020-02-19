c14800fb03bec09ed4958f15ca9114a1511badc5
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2020-02-18 20:56:55 -0600

    person object

---
+++ Person.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(name, email, phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+    greet(otherPerson) {
+      console.log('Hello ' + otherPerson.name + ', I am ' + this.name + '!');
+    }
+  }
+
+let Sonny = new Person ("Sonny", "sonny@hotmail.com", "515-515-5511")
